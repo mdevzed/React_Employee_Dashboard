a3bbc8ebdcbedd450cdb5f8af79a5d945862b7ca
KK-MSD <mdevzed@gmail.com>
2024-04-05 21:16:14 -0400

    made data.js

--- src/data/data.js
+++ src/data/data.js
@@ -1,3 +1,32 @@
 const data = [{
-    
-}]
+    "image": "profile1.png",
+    "id": "1",
+    "firstName": "Jeff",
+    "lastName": "Bobley",
+    "title": "Software Engineer"
+}, {
+    "image": "profile2.png",
+    "id": "2",
+    "firstName": "John",
+    "lastName": "Smith",
+    "title": "Product Manager"
+}, {
+    "image": "profile3.png",
+    "id": "3",
+    "firstName": "Jane",
+    "lastName": "Doe",
+    "title": "Marketing Manager"
+}, {
+    "image": "profile4.png",
+    "id": "4",
+    "firstName": "Michael",
+    "lastName": "Jones",
+    "title": "Sales Representative"
+}, {
+    "image": "profile5.png",
+    "id": "5",
+    "firstName": "Mary",
+    "lastName": "Johnson",
+    "title": "Customer Service Representative"
+
+}];
