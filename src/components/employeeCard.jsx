024fee57f53016fc7e8f5f77a1e9f235d7c300a7
KK-MSD <mdevzed@gmail.com>
2024-04-12 19:53:38 -0400

    updated employee card, centered image, created stacks of chips

--- src/components/employeeCard.jsx
+++ src/components/employeeCard.jsx
@@ -6,28 +6,39 @@ import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Face5Icon from '@mui/icons-material/Face5';
+import Box from '@mui/material/Box';
+import Chip from '@mui/material/Chip';
+import Stack from '@mui/material/Stack';
 
 export default function MediaCard() {
   return (
-    <Card sx={{ maxWidth: 365 }}>
-      <CardMedia
-        sx={{ height: 140 }}
+    <Card sx={{ maxWidth: 365, px: 2, margin:'auto'}}>
+      <Box sx={{alignItems: "center"}}>
+        <CardMedia
+        sx={{ height: 240, width: 240, backgroundSize: 'contain'}}
         image="profile1.png"
         title="Jeff Bobley"
-      />
-      <CardContent>
+        />
+      </Box>
+
+      <CardContent sx={{}}>
         <Face5Icon />
         <Typography gutterBottom variant="h5" component="div">
           Jeff Bobley
         </Typography>
         <Typography variant="body2" color="text.secondary">
-          Very strong swimmer. 
+          Department: Product Design
         </Typography>
+        <Stack direction="row" spacing={1} my={1}>
+          <Chip label="Manager" variant="outlined" sx={{fontSize: '.75rem'}}/>
+          <Chip label="Figma" variant="outlined" sx={{fontSize: '.75rem'}} />
+          <Chip label="JavaScript" variant="outlined" sx={{fontSize: '.75rem'}} />
+        </Stack>
+        <Stack direction="row" spacing={1} my={1} justifyContent="center">
+          <Chip label="NY" variant="outlined" sx={{fontSize: '.75rem'}}/>
+          <Chip label="Full-Time" variant="outlined" sx={{fontSize: '.75rem'}} />
+        </Stack>
       </CardContent>
-      <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
-      </CardActions>
     </Card>
   );
 }
